18ca73966002eba4ffc5bb920e4036db7dd7941c
Kabir <sagithedeveloper@gmail.com>
2024-04-19 08:04:55 +0530

    Added the code

---
+++ S13_19-04-2024/src/App.js
@@ -0,0 +1,28 @@
+import "./App.css";
+import Header from "./Header";
+
+function App() {
+  return (
+    <div id="app">
+      <h2 id="title">App Component</h2>
+      <p>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Tempore quia
+        reprehenderit libero laudantium incidunt numquam autem doloribus
+        molestiae quisquam a hic, mollitia aspernatur quis rerum beatae ad totam
+        quae minus voluptatibus suscipit velit aliquam expedita maiores
+        explicabo. Aut excepturi veritatis mollitia commodi quibusdam, eligendi
+        beatae, reiciendis, explicabo rem delectus alias. Laboriosam unde
+        provident velit consectetur at, exercitationem molestias. Ex voluptatem
+        cupiditate sit nam sequi corrupti exercitationem voluptates quia nostrum
+        non debitis temporibus voluptate qui laboriosam iure doloremque magni
+        consectetur illum, architecto minus suscipit aut ad natus esse! Modi ex
+        impedit libero amet eum quos quisquam similique dolorem expedita.
+        Suscipit, repudiandae.
+      </p>
+      <Header />
+      <Header />
+    </div>
+  );
+}
+
+export default App;
