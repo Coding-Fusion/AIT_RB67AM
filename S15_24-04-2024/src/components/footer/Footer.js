370a90d3a1924216291161e2b95263fb148b8ead
Kabir <sagithedeveloper@gmail.com>
2024-04-24 08:01:41 +0530

    added the code

---
+++ S15_24-04-2024/src/components/footer/Footer.js
@@ -0,0 +1,24 @@
+import "./Footer.css";
+function Footer() {
+  return (
+    <div id="footer">
+      <h2>Footer Component</h2>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum beatae,
+        aliquid accusantium deleniti, dolorem explicabo accusamus aliquam
+        reprehenderit veritatis impedit minus nostrum tempora. Sit officiis
+        similique repellat quibusdam vero vitae sapiente maiores id delectus
+        magni, quasi hic laudantium esse, temporibus ut nesciunt cumque quia
+        tenetur dolores, dicta quis modi perspiciatis. Iure minima dignissimos
+        perspiciatis voluptas, dolores tempore suscipit libero officiis
+        doloremque nihil odit excepturi animi voluptate qui mollitia magnam iste
+        corporis ipsum reiciendis repellat blanditiis totam numquam? Doloribus
+        quos cum eos deleniti inventore vitae aliquam veritatis a velit magni
+        quasi numquam maiores id nostrum odit rerum necessitatibus, labore
+        deserunt ad?
+      </p>
+    </div>
+  );
+}
+
+export default Footer;
